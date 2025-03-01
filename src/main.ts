4857a020d7092c481d23a42d760a3d509a258f77
ilyes <ilyes.hbibi999@gmail.com>
2025-03-01 22:27:57 +0100

    page login

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { HomeComponent } from './app/home/home.component';
+import { provideRouter } from '@angular/router';
+const routes = [
+  { path: '', component: HomeComponent }, // Default route
+  
+];
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).catch(err => console.error(err));
